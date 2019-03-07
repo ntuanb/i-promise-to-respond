19c92f74aa5ee07aa0f2e967ca14fbb7cd7b91ee
Tuan Bui <tuan.bui@intrepidtravel.com>
2019-03-07 17:41:57 +1100

    Update.

--- 04.js
+++ 04.js
@@ -3,6 +3,10 @@
 /**
   Notes
   - Works with promises
+  -  Javascript will resolve the promise and then move 
+  on to the next line. In a nutshell, we change the 
+  asynchronous code to read like synchronous code while 
+  still running asynchronously.
  */
 
 function _fetch() {
@@ -15,6 +19,27 @@ async function getSomethingFromPublicApi() {
   let result = await _fetch();
   console.log(result);
   console.log('Done');
+}
+
+getSomethingFromPublicApi();
+
+/**
+ * Try catch
+ */
+
+
+function _fetch2() {
+  return new Promise((resolve, reject) => {
+    reject('{ "error": "Something happened..." }');
+  })
+}
+
+async function getSomethingFromPublicApi() {
+  try {
+    let result = await _fetch();
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 getSomethingFromPublicApi();
